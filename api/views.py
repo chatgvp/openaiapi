766d3e7eb6b8077f0c975f8fcce77689bb2fb3e2
George Vincent Peña <gpena8826@gmail.com>
2023-07-27 21:45:12 +0800

    update

--- api/views.py
+++ api/views.py
@@ -5,37 +5,42 @@ import openai
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-api_key = "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"
+api_key = "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"
 openai.api_key = api_key
 
 
-def algorithm(job_title, job_qualifications, candidate1, candidate2):
+def algorithm(job_title, job_qualifications, candidate1):
     json_format = """
-    {
-    "job_title": "",
-    "job_qualifications": "qualifications you just enter",
-    "candidate1": {
-        "name":"",
-        "strengths": [],
-        "qualification_percentage": ""
-    },"candidate2": {
-        "name": "",
-        "strengths": [],
-        "qualification_percentage": ""
-    }
-    "comparison": ""
-    }
-    """
+                {
+                "job_title": "",
+                "job_qualifications": "",
+                "candidate1": {
+                    "name":"",
+                    "strengths": [],
+                    "weaknesses": [],
+                    "qualification_percentage": ""
+                },
+                "summary": ""
+                }
+                """
+
     chatgpt_prompt = (
-        "Ensure the candidates are related to the Job Title and Job Qualification. If not, make the qualifications percentage very low and, in comparison, not qualified for the job."
-        + candidate1
-        + " compare to "
-        + candidate2
-        + " in the Position of: "
+        "Evaluate the candidate's qualifications for the position of "
         + job_title
-        + " Job Qualifications: "
+        + " based on the provided job qualifications.\n\n"
+        + "Candidate:\n"
+        + candidate1
+        + "\n\n"
+        + "Position: "
+        + job_title
+        + "\n\n"
+        + "Job Qualifications:"
         + job_qualifications
-        + "follow this JSON format"
+        + "\n\n"
+        + "If the candidate does not meet the qualifications: provide a detailed explanation of their strengths and weaknesses, and assign a low qualification percentage.\n"
+        + "Do not recommend the candidate for the position if they do not meet the minimum qualifications.\n If the candidate does meet the qualifications: Provide a detailed explanation of why they are well-suited for the role and assign a high qualification percentage."
+        + "Also provide a summary of the why the candidate got their qualification percentage limited to 280 characters"
+        + "Please completely follow and enter all fields using this JSON format:\n"
         + json_format
     )
 
@@ -53,26 +58,25 @@ def algorithm(job_title, job_qualifications, candidate1, candidate2):
 
 class PdfToJsonView(APIView):
     def post(self, request, *args, **kwargs):
-        if "candidate1" in request.FILES and "candidate2" in request.FILES:
+        print(request)
+        if "candidate1" in request.FILES:
+            num_files_uploaded = len(request.FILES)
+            print(num_files_uploaded)
+            print(request.FILES)
             job_title = request.POST.get("job_title")
             job_qualifications = request.POST.get("job_qualifications")
-
             candidate1 = request.FILES["candidate1"]
-            candidate2 = request.FILES["candidate2"]
             pdf_reader1 = PyPDF2.PdfReader(candidate1)
+            print(pdf_reader1)
+            # print(len(candidate1))
             candidate_text1 = ""
             for page in pdf_reader1.pages:
                 candidate_text1 += page.extract_text()
-            pdf_reader2 = PyPDF2.PdfReader(candidate2)
-            candidate_text2 = ""
-            for page in pdf_reader2.pages:
-                candidate_text2 += page.extract_text()
-            response_data = algorithm(
-                job_title, job_qualifications, candidate_text1, candidate_text2
-            )
-            cleaned_string = response_data.replace("\n", "")
-            json_object = json.loads(cleaned_string)
+            response_data = algorithm(job_title, job_qualifications, candidate_text1)
+            # cleaned_string = response_data.replace("\n", "")
+            json_object = json.loads(response_data)
             return Response(json_object, content_type="application/json")
+
         else:
             return Response(
                 {"error": "Both candidate1 and candidate2 PDF files must be uploaded."},
